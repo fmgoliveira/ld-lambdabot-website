439273fffd779ba9345353ea372422924a2ba7db
Francisco Oliveira <74855391+franciscomgoliveira@users.noreply.github.com>
2022-06-03 16:42:46 +0100

    continued fixing some bugs

--- backend/src/client/methods.ts
+++ backend/src/client/methods.ts
@@ -2,16 +2,16 @@ import { TextBasedChannel } from 'discord.js';
 import { PermissionString } from 'discord.js';
 import { client } from '.';
 
-export const checkForBotPermissionInChannel = (channelId: string, permission: PermissionString) => {
-  const channel = client.channels.cache.find(c => c.id === channelId && c.isText());
-  if (!channel) return 0;
+export const checkForBotPermissionInChannel = async (channelId: string, permission: PermissionString) => {
+  const channel = await client.channels.fetch(channelId);
+  if (!channel || !channel.isText()) return 0;
   if (channel.type !== 'GUILD_TEXT' && channel.type !== 'GUILD_NEWS') return 0;
 
   return channel.permissionsFor(process.env.DISCORD_CLIENT_ID!)?.has(permission) ? 2 : 1;
 }
 
-export const checkForBotPermissionInCategory = (categoryId: string, permission: PermissionString) => {
-  const channel = client.channels.cache.find(c => c.id === categoryId);
+export const checkForBotPermissionInCategory = async (categoryId: string, permission: PermissionString) => {
+  const channel = await client.channels.fetch(categoryId);
   if (!channel) return 0;
   if (channel.type !== 'GUILD_CATEGORY') return 0;
 
--- dist/server/client/methods.js
+++ dist/server/client/methods.js
@@ -2,17 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.checkForBotPermissionManageRole = exports.checkForBotPermissionInCategory = exports.checkForBotPermissionInChannel = void 0;
 const _1 = require(".");
-const checkForBotPermissionInChannel = (channelId, permission) => {
-    const channel = _1.client.channels.cache.find(c => c.id === channelId && c.isText());
-    if (!channel)
+const checkForBotPermissionInChannel = async (channelId, permission) => {
+    const channel = await _1.client.channels.fetch(channelId);
+    if (!channel || !channel.isText())
         return 0;
     if (channel.type !== 'GUILD_TEXT' && channel.type !== 'GUILD_NEWS')
         return 0;
     return channel.permissionsFor(process.env.DISCORD_CLIENT_ID)?.has(permission) ? 2 : 1;
 };
 exports.checkForBotPermissionInChannel = checkForBotPermissionInChannel;
-const checkForBotPermissionInCategory = (categoryId, permission) => {
-    const channel = _1.client.channels.cache.find(c => c.id === categoryId);
+const checkForBotPermissionInCategory = async (categoryId, permission) => {
+    const channel = await _1.client.channels.fetch(categoryId);
     if (!channel)
         return 0;
     if (channel.type !== 'GUILD_CATEGORY')
